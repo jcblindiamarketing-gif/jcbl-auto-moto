f1a45d80341233f92556d8f9c272d6d2ef5161a5
jcbl india marketing <jcblindiamarketing@gmail.com>
2026-06-22 10:30:42 +0530

    Updated project

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "JCBL Auto Moto",
   description: "JCBL India Auto Moto",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
